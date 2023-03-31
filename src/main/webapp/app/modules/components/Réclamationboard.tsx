0a352eb89a9edf6e123966e7db82399b45ab1fa5
mahmoudayachi <ayachimahmoud175@gmail.com>
2023-03-31 16:48:04 +0100

    réclamation board modified

--- src/main/webapp/app/modules/components/Réclamationboard.tsx
+++ src/main/webapp/app/modules/components/Réclamationboard.tsx
@@ -1,21 +1,34 @@
 import React from 'react';
 import './Réclamationboard.scss';
-import { Nav, NavItem, NavLink } from 'reactstrap';
+import { Nav, NavItem, NavLink, TabPane, Row, Container, TabContent } from 'reactstrap';
 import CityServiceCarte from './CityServiceCarte';
 import { useState } from 'react';
 
 const Réclamationboard = () => {
+  const [active, Setactive] = useState(false);
+  const [inactive, Setinaactive] = useState(true);
+  const change = () => {
+    if (active == false) {
+      Setinaactive(false);
+      Setactive(true);
+    } else if (inactive == false) {
+      Setinaactive(true);
+      Setactive(false);
+    }
+  };
   return (
     <>
       <div className="tab-container">
-        <Nav fill pills>
-          <NavItem>
-            <NavLink active href="#">
+        <Nav className="nav" fill pills>
+          <NavItem className="item">
+            <NavLink className="link" active={active} onClick={change} href="#">
               envoyer une réclamation
             </NavLink>
           </NavItem>
-          <NavItem>
-            <NavLink href="#">voir les réclamations</NavLink>
+          <NavItem className="item">
+            <NavLink className="link" active={inactive} onClick={change} href="#">
+              voir réclamation
+            </NavLink>
           </NavItem>
         </Nav>
       </div>
